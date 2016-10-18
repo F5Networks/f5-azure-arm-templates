0cbe7f935a24c42c43794aab2284d02d60de4af6
James Sevedge <j.sevedge@f5.com>
2016-10-18 08:39:22 -0700

    Add additional requirements, change URL to test

--- azure-arm-2nic/configuremultinic.sh
+++ azure-arm-2nic/configuremultinic.sh
@@ -4,8 +4,12 @@
 # Parameter 1: MGMT IP
 # Parameter 2: Default GW
 # Parameter 3: List of Additional IP's
-# Example for 3 nic: ./configuremultinic.sh 10.0.1.5 10.0.1.1 "10.0.2.5 10.0.3.5"
+# Parameter 4: BIG IP Username for REST Calls
+# Parameter 5: BIG IP Password for REST Calls
+# Example for 3 nic: ./configuremultinic.sh 10.0.1.5 10.0.1.1 "10.0.2.5 10.0.3.5" admin admin
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin/"
+user=$4
+passwd=$5
 
     echo "$(date +%c): Starting NIC Configuration for multi-NIC Deployment"
 ## Set DB Variables to allow for multi nic
@@ -32,7 +36,13 @@ export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin/"
             tmsh create net self self_$int address $nic/24 vlan vlan_$int allow-service default
             int=$((int+1))
         done
-    tmsh save sys config > /dev/null 2>&1 &
+    # Save Sys Config via REST
+    response_code=$(curl -sku $user:$passwd -w "%{http_code}" -X POST -H "Content-Type: application/json" https://localhost/mgmt/tm/sys/config -d '{"command":"save"}' -o /dev/null)
+
+    if [[ $response_code != 200  ]]; then
+        echo "$(date +%c): Failed to Save Sys Config; exiting."
+        exit
+    fi
 
     echo "$(date +%c): Ending NIC Configuration for multi-NIC Deployment"
     exit
