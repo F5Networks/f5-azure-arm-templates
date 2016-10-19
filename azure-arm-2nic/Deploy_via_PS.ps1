ca693c2f9317fb3f0035acacb4038b41bd5fa58f
James Sevedge <sevedge@olympus.f5net.com>
2016-10-19 15:04:10 -0700

    Minor tmpl change, add deploy script and parameters file

---
+++ azure-arm-2nic/Deploy_via_PS.ps1
@@ -0,0 +1,42 @@
+﻿#Start
+
+param(
+  [Parameter(Mandatory=$True)]
+  [string]
+  $deploymentName,
+
+  [Parameter(Mandatory=$True)]
+  [string]
+  $vmName,
+
+  [Parameter(Mandatory=$True)]
+  [string]
+  $licenseToken,
+
+  [string]
+  $f5pwd = "P4ssw0rd!azure",
+
+  [string]
+  $region = "West US",
+
+  [string]
+  $templateFilePath = "azuredeploy.json",
+
+  [string]
+  $parametersFilePath = "azuredeploy.parameters.json"
+)
+
+
+Write-Host Logging in...
+Add-AzureRmAccount
+
+
+New-AzureRmResourceGroup -Name $deploymentName -Location "$region"
+Write-Host Resource Group $deploymentName created in $region
+
+
+$pwd = ConvertTo-SecureString -String $f5pwd -AsPlainText -Force
+$deployment = New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $deploymentName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath -adminPassword $pwd -dnsLabelPrefix $deploymentName -vmName "$vmName" -licenseToken1 "$licensetoken"
+
+$deployment
+
