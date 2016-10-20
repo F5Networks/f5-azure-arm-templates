ccec0d2908b02cdd2fccc42965e7105afa20e93a
James Sevedge <j.sevedge@f5.com>
2016-10-20 14:58:27 -0700

    Changed Help entry

--- azure-arm-1nic/Deploy_via_PS.ps1
+++ azure-arm-1nic/Deploy_via_PS.ps1
@@ -47,16 +47,16 @@ $deployment
 
 #Send Email letting know if successful or not
 $status = $deployment.ProvisioningState
-$type = "f5-arm-2nic"
+$type = "f5-arm-1nic"
 
 $timestamp = get-date -format g
-$EmailFrom = "discoveryeselabsauto@gmail.com" 
-$Subject = "[$timestamp] Notification for Azure Build Complete[$status]" 
-$Body = "This is a notification for automated azure builds.. `n `n Testing template of type: $type " 
-$SMTPServer = "smtp.gmail.com" 
-$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
-$SMTPClient.EnableSsl = $true 
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("discoveryeselabsauto", "P4ssw0rd!azure"); 
+$EmailFrom = "discoveryeselabsauto@gmail.com"
+$Subject = "[$timestamp] Notification for Azure Build Complete[$status]"
+$Body = "This is a notification for automated azure builds.. `n `n Testing template of type: $type "
+$SMTPServer = "smtp.gmail.com"
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPClient.EnableSsl = $true
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("discoveryeselabsauto", "P4ssw0rd!azure");
 $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
 
 Write-Host "Email Has been Sent to $EmailTo at $timestamp"
