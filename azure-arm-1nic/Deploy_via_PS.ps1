4dedd8b56a70561104a4ba455fbb9fca2dbf3264
James Sevedge <j.sevedge@f5.com>
2016-10-20 12:58:33 -0700

    Check in base 1 nic template

---
+++ azure-arm-1nic/Deploy_via_PS.ps1
@@ -0,0 +1,65 @@
+﻿#Start
+
+param(
+  [Parameter(Mandatory=$True)]
+  [string]
+  $deploymentName,
+
+  [Parameter(Mandatory=$True)]
+  [string]
+  $vmName,
+
+  [Parameter(Mandatory=$True)]
+  [string]
+  $licenseToken,
+
+  [string]
+  $EmailTo = "j.sevedge@f5.com",
+
+  [string]
+  $f5pwd = "P4ssw0rd!azure",
+
+  [string]
+  $region = "West US",
+
+  [string]
+  $templateFilePath = "azuredeploy.json",
+
+  [string]
+  $parametersFilePath = "azuredeploy.parameters.json"
+)
+
+$timestamp = get-date -format g
+Write-Host "[$timestamp] Starting Script "
+
+#Connect to Azure, need to add automation capabilities
+Add-AzureRmAccount
+
+New-AzureRmResourceGroup -Name $deploymentName -Location "$region"
+Write-Host Resource Group $deploymentName created in $region
+
+
+$pwd = ConvertTo-SecureString -String $f5pwd -AsPlainText -Force
+$deployment = New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $deploymentName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath -adminPassword $pwd -dnsLabelPrefix $deploymentName -vmName "$vmName" -licenseToken1 "$licensetoken"
+
+$deployment
+
+
+#Send Email letting know if successful or not
+$status = $deployment.ProvisioningState
+$type = "f5-arm-2nic"
+
+$timestamp = get-date -format g
+$EmailFrom = "discoveryeselabsauto@gmail.com" 
+$Subject = "[$timestamp] Notification for Azure Build Complete[$status]" 
+$Body = "This is a notification for automated azure builds.. `n `n Testing template of type: $type " 
+$SMTPServer = "smtp.gmail.com" 
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
+$SMTPClient.EnableSsl = $true 
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("discoveryeselabsauto", "P4ssw0rd!azure"); 
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+
+Write-Host "Email Has been Sent to $EmailTo at $timestamp"
+
+Write-Host "[$timestamp] Ending Script"
+
