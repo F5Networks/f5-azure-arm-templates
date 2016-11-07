3a4b092a02050f12ab7d8d93019627ccfebbd6a6
James Sevedge <j.sevedge@f5.com>
2016-11-07 14:36:27 -0800

    Tweak Bash Script

--- experimental/cluster/1nic/deploy_via_bash.sh
+++ experimental/cluster/1nic/deploy_via_bash.sh
@@ -10,25 +10,66 @@ template_file="azuredeploy.json"
 parameter_file="azuredeploy.parameters.json"
 instance_type="Standard_D2_v2"
 image_name="Best"
+tag_values=""
 
-while getopts u:p:d:n:s:k:l:r:y:z: option
-do	case "$option"  in
-        w) solution_deployment_name=$OPTARG;;
-        h) number_of_instances=$OPTARG;;
-        u) admin_username=$OPTARG;;
-        p) admin_password=$OPTARG;;
-	    d) dns_label=$OPTARG;;
-        s) instance_type=$OPTARG;;
-        k) image_name=$OPTARG;;
-        l) license_key_1=$OPTARG;;
-        m) license_key_2=$OPTARG;;
-        r) resource_group_name=$OPTARG;;
-		y) azure_user=$OPTARG;;
-		z) azure_pwd=$OPTARG;;
+
+ARGS=`getopt -o a:b:c:d:e:f:g:h:i:j:k:l:m --long sdname:,nbrinstances:,adminusr:,adminpwd:,insttype:,imgname:,dnslabel:,key1:,key2:,rstsrcaddr:,rgname:,azureuser:,azurepwd: -n $0 -- "$@"`
+eval set -- "$ARGS"
+
+
+# Parse the command line arguments, only chacking full params
+while true; do
+    case "$1" in
+        -a|--sdname)
+            solution_deployment_name=$2
+            shift 2;;
+        -b|--nbrinstances)
+            number_of_instances=$2
+            shift 2;;
+        -c|--adminusr)
+            admin_username=$2
+            shift 2;;
+        -d|--adminpwd)
+            admin_password=$2
+            shift 2;;
+        -e|--insttype)
+            instance_type=$2
+            shift 2;;
+        -f|--imgname)
+            image_name=$2
+            shift 2;;
+        -g|--dnslabel)
+            dns_label=$2
+            shift 2;;
+        -h|--key1)
+            license_key_1=$2
+            shift 2;;
+        -i|--key2)
+            license_key_2=$2
+            shift 2;;
+        -j|--rstsrcaddr)
+            restricted_source_address=$2
+            shift 2;;
+        -k|--rgname)
+            resource_group_name=$2
+            shift 2;;
+        -l|--azureuser)
+            azure_user=$2
+            shift 2;;
+        -m|--azurepwd)
+            azure_pwd=$2
+            shift 2;;
+        --)
+            shift
+            break;;
     esac
 done
+
+
+echo "sdname: $sdname"
+
 # Check for Mandatory Args
-if [ ! "$admin_username" ] || [ ! "$admin_password" ] || [ ! "$dns_label" ] || [ ! "$license_key_1" ] || [ ! "$resource_group_name" ] || [ ! "$azure_user" ] || [ ! "$azure_pwd" ]
+if [ ! "$solution_deployment_name" ] || [ ! "$number_of_instances" ] || [ ! "$admin_username" ] || [ ! "$admin_password" ] || [ ! "$license_key_1" ]  || [ ! "$license_key_2" ] || [ ! "$restricted_source_address" ] || [ ! "$resource_group_name" ] || [ ! "$azure_user" ] || [ ! "$azure_pwd" ]
 then
     echo "One of the mandatory parameters was not specified!"
     exit 1
@@ -46,7 +87,7 @@ azure group create -n $resource_group_name -l $region
 
 # Deploy ARM Template, right now cannot specify parameter file AND parameters inline via Azure CLI,
 # such as can been done with Powershell...oh well!
-azure group deployment create -f $template_file -g $resource_group_name -n $resource_group_name -p "{\"adminUsername\":{\"value\":\"$admin_username\"},\"adminPassword\":{\"value\":\"$admin_password\"},\"dnsLabel\":{\"value\":\"$dns_label\"},\"instanceType\":{\"value\":\"$instance_type\"},\"licenseKey1\":{\"value\":\"$license_key_1\"},\"iimageName\":{\"value\":\"$image_name\"}}"
+azure group deployment create -f $template_file -g $resource_group_name -n $resource_group_name -p "{\"solutionDeploymentName\":{\"value\":\"$solution_deployment_name\"},\"numberOfInstances\":{\"value\":\"$number_of_instances\"},\"instanceType\":{\"value\":\"$instance_type\"},\"imageName\":{\"value\":\"$image_name\"},\"adminUsername\":{\"value\":\"$admin_username\"},\"adminPassword\":{\"value\":\"$admin_password\"},\"dnsLabel\":{\"value\":\"$dns_label\"},\"licenseKey1\":{\"value\":\"$license_key_1\"},\"licenseKey2\":{\"value\":\"$license_key_2\"},\"restrictedSrcAddress\":{\"value\":\"$restricted_source_address\"},\"tagValues\":{\"value\":\"$tag_values\"}}"
 
 
 
