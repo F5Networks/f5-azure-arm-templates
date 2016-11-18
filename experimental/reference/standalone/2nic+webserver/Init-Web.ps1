3997e8753bd28aeaa70922b09ecf5ac0937ee7ed
James Sevedge <j.sevedge@f5.com>
2016-11-18 11:09:27 -0800

    Update to 2nic+webserver

--- experimental/reference/standalone/2nic+webserver/Init-Web.ps1
+++ experimental/reference/standalone/2nic+webserver/Init-Web.ps1
@@ -1,6 +1,11 @@
 # PowerShell Script to Initialize Webserver for 2nic+webserver Demo
 
-Write-Host "Installing Web Server Role"
 $webinstall = Install-WindowsFeature -Name "Web-Server" -IncludeAllSubFeature
-
 Write-Host "Result of Installing: $webinstall"
+
+# Download DWeb Page(s) to display, windows by default will accept Default.htm in directory specificed as the file to server
+$sourcehtml = "https://raw.githubusercontent.com/F5Networks/f5-azure-arm-templates/master/experimental/reference/standalone/2nic%2Bwebserver/Default.htm"
+$destinationhtml = "C:\inetpub\wwwroot\Default.htm"
+
+$downloadhtml = Invoke-WebRequest $sourcehtml -OutFile $destinationhtml
+Write-Host "Download Result: $downloadhtml"
