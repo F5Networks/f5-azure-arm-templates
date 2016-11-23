0e7ce91b13aad2a0c394c6377313d9694e04fb6c
James Sevedge <j.sevedge@f5.com>
2016-11-23 09:25:25 -0800

    Modifed to support multiple pool mbrs

--- experimental/reference/standalone/2nic+webserver/deploy_iapp_http.sh
+++ experimental/reference/standalone/2nic+webserver/deploy_iapp_http.sh
@@ -9,7 +9,7 @@ do	case "$option"  in
         u) user=$OPTARG;;
         p) passwd=$OPTARG;;
 		v) vs_ip=$OPTARG;;
-    esac 
+    esac
 done
 
 IP_REGEX='^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$'
@@ -18,12 +18,12 @@ mode="http"
 device_group="none"
 traffic_group="traffic-group-local-only"
 
-sleep 60
+sleep 30
 
 # download iApp templates
 template_location="http://cdn-prod-ore-f5.s3-website-us-west-2.amazonaws.com/product/blackbox/staging/azure"
 
-#In a for loop to allow for adding additional template downloads
+# In a for loop to allow for adding additional template downloads
 for template in f5.http.v1.2.0rc4.tmpl
 do
      curl -k -s -f --retry 5 --retry-delay 10 --retry-max-time 10 -o /config/$template $template_location/$template
@@ -35,23 +35,30 @@ do
      sleep 10
 done
 
-
 # pre-create node
-if [[ $pool_member =~ $IP_REGEX ]]; then
-     response_code=$(curl -sku $user:$passwd -w "%{http_code}" -X POST -H "Content-Type: application/json" https://localhost/mgmt/tm/ltm/node -d '{"name": "'"$pool_member"'","partition": "Common","address": "'"$pool_member"'"}' -o /dev/null)
-else
-     response_code=$(curl -sku $user:$passwd -w "%{http_code}" -X POST -H "Content-Type: application/json" https://localhost/mgmt/tm/ltm/node -d '{"name": "'"$pool_member"'","partition": "Common","fqdn": {"autopopulate": "enabled","tmName": "'"$pool_member"'"}}' -o /dev/null)
-fi
-
-if [[ $response_code != 200  ]]; then
-     echo "Failed to create node; with response code '"$response_code"'"
-fi
-
+for unique_node in $pool_member
+do
+    if [[ $unique_node =~ $IP_REGEX ]]; then
+        response_code=$(curl -sku $user:$passwd -w "%{http_code}" -X POST -H "Content-Type: application/json" https://localhost/mgmt/tm/ltm/node -d '{"name": "'"$unique_node"'","partition": "Common","address": "'"$unique_node"'"}' -o /dev/null)
+    else
+        response_code=$(curl -sku $user:$passwd -w "%{http_code}" -X POST -H "Content-Type: application/json" https://localhost/mgmt/tm/ltm/node -d '{"name": "'"$unique_node"'","partition": "Common","fqdn": {"autopopulate": "enabled","tmName": "'"$unique_node"'"}}' -o /dev/null)
+    fi
+    if [[ $response_code != 200  ]]; then
+        echo "Failed to create node $unique_node; with response code '"$response_code"'"
+    fi
+done
 sleep 10
 
+# Create json blob of pool members
+for unique_member in $pool_member
+do
+    pool_members_json+='{"row":["'"$unique_member"'","'"80"'","0"]},'
+done
+
+
 # deploy unencrypted application
-if [[ $mode == "http"  ]]; then        
-     response_code=$(curl -sku $user:$passwd -w "%{http_code}" -X POST -H "Content-Type: application/json" https://localhost/mgmt/tm/sys/application/service/ -d '{"name":"'"$deployment"'-'"$vs_http_port"'","partition":"Common","deviceGroup":"'"$device_group"'","strictUpdates":"disabled","template":"/Common/f5.http.v1.2.0rc4","trafficGroup":"'"$traffic_group"'","tables":[{"name":"pool__hosts","columnNames":["name"],"rows":[{"row":["'"$deployment"'"]}]},{"name":"pool__members","columnNames":["addr","port","connection_limit"],"rows":[{"row":["'"$pool_member"'","'"$pool_http_port"'","0"]}]},{"name":"server_pools__servers"}],"variables":[{"name":"monitor__monitor","encrypted":"no","value":"/Common/http"},{"name":"pool__addr","encrypted":"no","value":"'"$vs_ip"'"},{"name":"pool__mask","encrypted":"no","value":"255.255.255.255"},{"name":"pool__persist","encrypted":"no","value":"/#cookie#"},{"name":"pool__port","encrypted":"no","value":"'"$vs_http_port"'"},{"name":"ssl__mode","encrypted":"no","value":"no_ssl"}]}' -o /dev/null)
+if [[ $mode == "http"  ]]; then
+     response_code=$(curl -sku $user:$passwd -w "%{http_code}" -X POST -H "Content-Type: application/json" https://localhost/mgmt/tm/sys/application/service/ -d '{"name":"'"$deployment"'-'"$vs_http_port"'","partition":"Common","deviceGroup":"'"$device_group"'","strictUpdates":"disabled","template":"/Common/f5.http.v1.2.0rc4","trafficGroup":"'"$traffic_group"'","tables":[{"name":"pool__hosts","columnNames":["name"],"rows":[{"row":["'"$deployment"'"]}]},{"name":"pool__members","columnNames":["addr","port","connection_limit"],"rows":['"${pool_members_json%,}"']},{"name":"server_pools__servers"}],"variables":[{"name":"monitor__monitor","encrypted":"no","value":"/Common/http"},{"name":"pool__addr","encrypted":"no","value":"'"$vs_ip"'"},{"name":"pool__mask","encrypted":"no","value":"255.255.255.255"},{"name":"pool__persist","encrypted":"no","value":"/#cookie#"},{"name":"pool__port","encrypted":"no","value":"'"$vs_http_port"'"},{"name":"ssl__mode","encrypted":"no","value":"no_ssl"}]}' -o /dev/null)
 
      if [[ $response_code != 200  ]]; then
           echo "Failed to deploy unencrypted application; exiting with response code '"$response_code"'"
