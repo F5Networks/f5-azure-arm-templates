29b4849345b35898af045ea8d501ce531a09ee63
James Sevedge <j.sevedge@f5.com>
2017-02-08 09:13:47 -0800

    Update build script

--- build/build_arm.sh
+++ build/build_arm.sh
@@ -10,3 +10,7 @@ python '.\master_template.py' --template-name 2nic_limited --license-type BYOL -
 python '.\master_template.py' --template-name 1nic --license-type BYOL --template-location '../supported/standalone/1nic/'
 
 python '.\master_template.py' --template-name 2nic_limited --license-type BYOL --template-location '../supported/standalone/2nic_limited/'
+
+# BIGIP ARM Templates - Cluster (base)
+# Test Location
+python '.\master_template.py' --template-name cluster_base --license-type BYOL --template-location './testing_built_templates/cluster/1nic/'
