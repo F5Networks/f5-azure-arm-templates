f2fedb8c5ca7b6df29e87b35f85ead75062ae4e1
James Sevedge <j.sevedge@f5.com>
2017-02-08 08:35:23 -0800

    Update test tmpl loc

--- build/build_arm.sh
+++ build/build_arm.sh
@@ -6,9 +6,7 @@
 python '.\master_template.py' --template-name 1nic --license-type BYOL --template-location '../experimental/standalone/1nic/'
 
 python '.\master_template.py' --template-name 2nic_limited --license-type BYOL --template-location '../experimental/standalone/2nic_limited/'
-
 # Supported
-
 python '.\master_template.py' --template-name 1nic --license-type BYOL --template-location '../supported/standalone/1nic/'
 
 python '.\master_template.py' --template-name 2nic_limited --license-type BYOL --template-location '../supported/standalone/2nic_limited/'
