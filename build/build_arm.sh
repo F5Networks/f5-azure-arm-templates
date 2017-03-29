90ecca8047248511b6879d79e501c5853cbc82ea
James Sevedge <j.sevedge@f5.com>
2017-03-29 14:45:44 -0700

    rename 3nic, add dfl gw

--- build/build_arm.sh
+++ build/build_arm.sh
@@ -1,35 +1,42 @@
 #/bin/bash
 
-## BIGIP ARM Templates - Standalone (1nic, 2nic_limited)
-# Experimental
+#### Experimental ####
+## BIGIP ARM Templates - Standalone (1nic, 2nic_limited, 3nic)
 python '.\master_template.py' --template-name 1nic --license-type PAYG --template-location '../experimental/standalone/1nic/PAYG/' --script-location '../experimental/standalone/1nic/'
 python '.\master_template.py' --template-name 1nic --license-type BYOL --template-location '../experimental/standalone/1nic/BYOL/' --script-location '../experimental/standalone/1nic/'
 
 python '.\master_template.py' --template-name 2nic_limited --license-type PAYG --template-location '../experimental/standalone/2nic_limited/PAYG/' --script-location '../experimental/standalone/2nic_limited/'
 python '.\master_template.py' --template-name 2nic_limited --license-type BYOL --template-location '../experimental/standalone/2nic_limited/BYOL/' --script-location '../experimental/standalone/2nic_limited/'
 
-python '.\master_template.py' --template-name 3_nic --license-type PAYG --template-location '../experimental/standalone/3_nic/PAYG/' --script-location '../experimental/standalone/3_nic/'
-python '.\master_template.py' --template-name 3_nic --license-type BYOL --template-location '../experimental/standalone/3_nic/BYOL/' --script-location '../experimental/standalone/3_nic/'
+python '.\master_template.py' --template-name 3nic --license-type PAYG --template-location '../experimental/standalone/3nic/PAYG/' --script-location '../experimental/standalone/3nic/'
+python '.\master_template.py' --template-name 3nic --license-type BYOL --template-location '../experimental/standalone/3nic/BYOL/' --script-location '../experimental/standalone/3nic/'
 
-# Supported
+## BIGIP ARM Templates - Cluster (base)
+python '.\master_template.py' --template-name cluster_base --license-type PAYG --template-location '../experimental/cluster/1nic/PAYG/' --script-location '../experimental/cluster/1nic/'
+python '.\master_template.py' --template-name cluster_base --license-type BYOL --template-location '../experimental/cluster/1nic/BYOL/' --script-location '../experimental/cluster/1nic/'
+
+## BIGIP ARM Template - LTM AutoScale
+python '.\master_template.py' --template-name ltm_autoscale --license-type PAYG --template-location '../experimental/autoscale/ltm/' --script-location '../experimental/autoscale/ltm/' --solution-location 'experimental'
+
+## BIGIP ARM Template - WAF AutoScale
+python '.\master_template.py' --template-name waf_autoscale --license-type PAYG --template-location '../experimental/autoscale/waf/' --script-location '../experimental/autoscale/waf/' --solution-location 'experimental'
+
+#### End Experimental ####
+
+
+#### Supported ####
+## BIGIP ARM Templates - Standalone (1nic, 2nic_limited, 3nic)
 python '.\master_template.py' --template-name 1nic --license-type PAYG --template-location '../supported/standalone/1nic/PAYG/' --script-location '../supported/standalone/1nic/'
 python '.\master_template.py' --template-name 1nic --license-type BYOL --template-location '../supported/standalone/1nic/BYOL/' --script-location '../supported/standalone/1nic/'
 
 python '.\master_template.py' --template-name 2nic_limited --license-type PAYG --template-location '../supported/standalone/2nic_limited/PAYG/' --script-location '../supported/standalone/2nic_limited/'
 python '.\master_template.py' --template-name 2nic_limited --license-type BYOL --template-location '../supported/standalone/2nic_limited/BYOL/' --script-location '../supported/standalone/2nic_limited/'
 
+python '.\master_template.py' --template-name 3nic --license-type PAYG --template-location '../supported/standalone/3nic/PAYG/' --script-location '../supported/standalone/3nic/'
+python '.\master_template.py' --template-name 3nic --license-type BYOL --template-location '../supported/standalone/3nic/BYOL/' --script-location '../supported/standalone/3nic/'
+
 ## BIGIP ARM Templates - Cluster (base)
-# Experimental
-python '.\master_template.py' --template-name cluster_base --license-type PAYG --template-location '../experimental/cluster/1nic/PAYG/' --script-location '../experimental/cluster/1nic/'
-python '.\master_template.py' --template-name cluster_base --license-type BYOL --template-location '../experimental/cluster/1nic/BYOL/' --script-location '../experimental/cluster/1nic/'
-# Supported
 python '.\master_template.py' --template-name cluster_base --license-type PAYG --template-location '../supported/cluster/1nic/PAYG/' --script-location '../supported/cluster/1nic/'
 python '.\master_template.py' --template-name cluster_base --license-type BYOL --template-location '../supported/cluster/1nic/BYOL/' --script-location '../supported/cluster/1nic/'
 
-## BIGIP ARM Template - LTM AutoScale
-# Experimental
-python '.\master_template.py' --template-name ltm_autoscale --license-type PAYG --template-location '../experimental/autoscale/ltm/' --script-location '../experimental/autoscale/ltm/'
-
-## BIGIP ARM Template - LTM AutoScale
-# Experimental
-python '.\master_template.py' --template-name waf_autoscale --license-type PAYG --template-location '../experimental/autoscale/waf/' --script-location '../experimental/autoscale/waf/' --solution-location 'experimental'
+#### End Supported ####
