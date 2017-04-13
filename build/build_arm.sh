8ebf51d84db5058e1b07051a9b7da5b399b724f9
James Sevedge <j.sevedge@f5.com>
2017-04-13 08:55:22 -0700

    add new/existing stack to exp 3nic

--- build/build_arm.sh
+++ build/build_arm.sh
@@ -8,8 +8,10 @@ python '.\master_template.py' --template-name 1nic --license-type BYOL --templat
 python '.\master_template.py' --template-name 2nic --license-type PAYG --template-location '../experimental/standalone/2nic/PAYG/' --script-location '../experimental/standalone/2nic/'
 python '.\master_template.py' --template-name 2nic --license-type BYOL --template-location '../experimental/standalone/2nic/BYOL/' --script-location '../experimental/standalone/2nic/'
 
-python '.\master_template.py' --template-name 3nic --license-type PAYG --template-location '../experimental/standalone/3nic/PAYG/' --script-location '../experimental/standalone/3nic/'
-python '.\master_template.py' --template-name 3nic --license-type BYOL --template-location '../experimental/standalone/3nic/BYOL/' --script-location '../experimental/standalone/3nic/'
+python '.\master_template.py' --template-name 3nic --license-type PAYG --stack-type 'new' --template-location '../experimental/standalone/3nic/new_stack/PAYG/' --script-location '../experimental/standalone/3nic/new_stack/'
+python '.\master_template.py' --template-name 3nic --license-type BYOL --stack-type 'new' --template-location '../experimental/standalone/3nic/new_stack/BYOL/' --script-location '../experimental/standalone/3nic/new_stack/'
+python '.\master_template.py' --template-name 3nic --license-type PAYG --stack-type 'existing' --template-location '../experimental/standalone/3nic/existing_stack/PAYG/' --script-location '../experimental/standalone/3nic/existing_stack/'
+python '.\master_template.py' --template-name 3nic --license-type BYOL --stack-type 'existing' --template-location '../experimental/standalone/3nic/existing_stack/BYOL/' --script-location '../experimental/standalone/3nic/existing_stack/'
 
 ## BIGIP ARM Templates - Cluster (base)
 python '.\master_template.py' --template-name cluster_base --license-type PAYG --template-location '../experimental/cluster/1nic/PAYG/' --script-location '../experimental/cluster/1nic/'
