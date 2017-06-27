268ca1e39cd30a7a15a08f780b71d73c01f7eb8e
James Sevedge <j.sevedge@f5.com>
2017-06-27 10:36:59 -0700

    Update contentversion, release prep

--- build/master_template.py
+++ build/master_template.py
@@ -36,8 +36,8 @@ nic_reference = ""
 command_to_execute = ""
 
 ## Static Variable Assignment ##
-content_version = '3.2.0.0'
-f5_networks_tag = 'v3.2.0.0'
+content_version = '3.2.1.0'
+f5_networks_tag = 'v3.2.1.0'
 f5_cloud_libs_tag = 'v3.1.1'
 f5_cloud_libs_azure_tag = 'v1.2.0'
 f5_cloud_iapps_tag = 'v1.0.0'
