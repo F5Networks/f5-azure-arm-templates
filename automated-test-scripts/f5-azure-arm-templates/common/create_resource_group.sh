9be24a644da718f249eb54328366006a2fa7450c
shimkus <m.shimkus@f5.com>
2022-06-07 22:34:07 +0000

    Release commited to v10.0.0.0 tag

--- automated-test-scripts/f5-azure-arm-templates/common/create_resource_group.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "Succeeded"
-#  scriptTimeout = 15
-#  replayEnabled = false
-#  replayTimeout = 0
-
-az group create --location <REGION> --name <RESOURCE GROUP> --tags creator=dewdrop delete=True
